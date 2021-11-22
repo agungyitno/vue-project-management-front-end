0faa3e8d2a5400801bcc40c47bb1a57c7e8e96b4
Agung Prayitno <agungprayitno@users.noreply.github.com>
2021-11-22 12:00:40 +0700

    Initiate main js

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,42 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view />
 </template>
-
+<script>
+import { onMounted } from "vue";
+import { useStore } from "vuex";
+import { useRouter } from "vue-router";
+import axios from "axios";
+export default {
+  setup() {
+    const store = useStore();
+    const router = useRouter();
+    const token = localStorage.getItem("token");
+    onMounted(() => {
+      if (token) {
+        store.commit("setToken", token);
+        store.dispatch("getLogged");
+      }
+      axios.interceptors.response.use(
+        (response) => response,
+        (error) => {
+          if (error.response.status === 401) {
+            store.dispatch("forceLogout");
+            router.push("/login");
+          }
+          return Promise.reject(error);
+        }
+      );
+    });
+    return {
+      token,
+    };
+  },
+};
+</script>
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+@import "./assets/scss/tabler";
+.bi {
+  width: 1.25rem !important;
+  height: 1.25rem !important;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
-createApp(App).use(store).use(router).mount("#app");
+import BootstrapIcon from "@dvuckovic/vue3-bootstrap-icons";
+import axios from "axios";
+window.axios = axios;
+const app = createApp(App);
+app.use(store).use(router);
+app.component("bi", BootstrapIcon);
+app.mount("#app");
