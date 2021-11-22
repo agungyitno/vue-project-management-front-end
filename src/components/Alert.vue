915bd771b6536ed6bbe191ff1790eb5125e0432e
Agung Prayitno <agungprayitno@users.noreply.github.com>
2021-11-22 11:59:46 +0700

    make component for handle alert

---
+++ src/components/Alert.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="alert d-flex align-items-center p-1" :class="`alert-${type}`">
+    <span class="bg-transparent avatar" :class="`text-${type}`"
+      ><bi icon="exclamation-circle"></bi
+    ></span>
+    <span class="font-weight-medium">{{ message }}</span>
+  </div>
+</template>
+<script>
+export default {
+  name: "alert",
+  props: {
+    type: {
+      type: String,
+      default: "primary",
+    },
+    message: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
