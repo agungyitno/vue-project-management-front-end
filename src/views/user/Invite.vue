03f11b1b2c7d313b397b98a3fcef52f449429166
Agung Prayitno <agungprayitno@users.noreply.github.com>
2021-11-26 14:49:59 +0700

    Update dir name form users to user

---
+++ src/views/user/Invite.vue
@@ -0,0 +1,95 @@
+<template>
+  <div
+    class="modal modal-blur fade show"
+    id="modal-team"
+    tabindex="-1"
+    style="display: block"
+    aria-modal="true"
+    role="dialog"
+  >
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="progress card-progress" v-if="isLoading">
+          <div class="progress-bar progress-bar-indeterminate bg-primary">
+            <span class="visually-hidden">Loading...</span>
+          </div>
+        </div>
+        <div class="modal-header">
+          <h5 class="modal-title">Invite new user</h5>
+          <router-link :to="{ name: 'user' }" class="btn-close"></router-link>
+        </div>
+        <form @submit.prevent="handleSubmit">
+          <div class="modal-body">
+            <alert type="danger" :message="failed" v-if="failed"></alert>
+            <text-field
+              label="Email"
+              v-model="email"
+              :error="invalid.email ? invalid.email[0] : ''"
+            ></text-field>
+          </div>
+          <div class="modal-footer">
+            <router-link :to="{ name: 'user' }" class="btn me-auto">
+              Cancel
+            </router-link>
+            <button
+              type="submit"
+              class="btn btn-primary"
+              :class="{ disabled: isLoading }"
+            >
+              Invite
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { ref } from "@vue/reactivity";
+import textField from "../../components/field/Text";
+import alert from "../../components/Alert";
+import axios from "axios";
+import { useRouter } from "vue-router";
+export default {
+  name: "User Invite",
+  components: {
+    textField,
+    alert,
+  },
+  setup(_, { emit }) {
+    const router = useRouter();
+    const email = ref("");
+    const invalid = ref([]);
+    const failed = ref("");
+    const isLoading = ref(false);
+    const handleSubmit = () => {
+      isLoading.value = true;
+      invalid.value = {};
+      failed.value = "";
+      axios
+        .post("/user", { email: email.value })
+        .then((res) => {
+          console.log(res);
+          isLoading.value = false;
+          emit("updateUser");
+          router.push({ name: "user" });
+        })
+        .catch((err) => {
+          if (err.response.status == 422) {
+            invalid.value = err.response.data;
+          } else if (err.response.status == 404) {
+            failed.value = err.response.data.message;
+          }
+          isLoading.value = false;
+        });
+    };
+    return {
+      email,
+      invalid,
+      failed,
+      isLoading,
+      handleSubmit,
+    };
+  },
+};
+</script>
---
+++ src/views/user/Skeleton.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="col-md-6 col-xl-3" v-for="i in 4" :key="i">
+    <div class="card">
+      <div class="card-body">
+        <div class="row mb-2 justify-content-between">
+          <div class="col-4">
+            <div class="mt-0">
+              <skeletor />
+            </div>
+          </div>
+          <div class="col-2">
+            <skeletor />
+          </div>
+        </div>
+        <div class="text-center">
+          <div class="mb-3">
+            <skeletor circle size="7rem" />
+          </div>
+          <div class="card-title mb-1 text-center">
+            <skeletor heading />
+          </div>
+          <skeletor center width="80%" />
+        </div>
+      </div>
+      <div class="card-footer">
+        <div class="row text-center">
+          <div class="col">
+            <div class="small">
+              <skeletor />
+            </div>
+            <div class="h3">
+              <skeletor />
+            </div>
+          </div>
+          <div class="col">
+            <div class="small">
+              <skeletor />
+            </div>
+            <div class="h3">
+              <skeletor />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import skeletor from "../../components/Skeletor";
+
+export default {
+  components: { skeletor },
+};
+</script>
