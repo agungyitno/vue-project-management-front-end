ab2edc06da72f0d537aab6db6408bc2307e29708
Agung Prayitno <agungprayitno@users.noreply.github.com>
2021-11-26 14:52:47 +0700

    Update

---
+++ src/components/Skeletor.vue
@@ -0,0 +1,39 @@
+<template>
+  <div :style="{ width: width }" :class="{ 'mx-auto': center }">
+    <div
+      :class="{
+        'skeleton-avatar': circle,
+        'skeleton-heading': heading,
+        'skeleton-line': !heading && !circle,
+      }"
+      :style="[circle ? { width: size, height: size } : {}]"
+    ></div>
+  </div>
+</template>
+<script>
+export default {
+  nama: "skeletor",
+  props: {
+    circle: {
+      type: Boolean,
+      default: false,
+    },
+    heading: {
+      type: Boolean,
+      default: false,
+    },
+    center: {
+      type: Boolean,
+      default: false,
+    },
+    size: {
+      type: String,
+      default: "inherit",
+    },
+    width: {
+      type: String,
+      default: "100%",
+    },
+  },
+};
+</script>
